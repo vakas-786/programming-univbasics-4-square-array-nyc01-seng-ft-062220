4b8971b06601fded4350a4222f675a72a628368c
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-09 22:30:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   index = []
   new_array = []
   while counter < array.length do
-    new_array << (index_value ** 2)
+    new_array << (index ** 2)
     counter += 1
   end
   new_array
